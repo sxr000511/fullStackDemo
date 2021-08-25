069b6c69731ec7cdd6202cc885781cb59735fecd
sxr <240217843@qq.com>
2021-08-25 22:28:33 +0800

    1

--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -99,7 +99,7 @@ module.exports = (app) => {
     upload.single("file"),
     async (req, res) => {
       const file = req.file;
-      file.url = `http://localhost:3000/uploads/${file.filename}`;
+      file.url = `http://8.210.55.213/uploads/${file.filename}`;
       res.send(file);
     }
   );
